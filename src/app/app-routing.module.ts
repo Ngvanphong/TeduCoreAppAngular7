50ee8cb9055b7966c487ccaefa2ca2a270471085
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-10-30 16:15:42 +0700

    181030_v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login', pathMatch:'full'},
+  {path:'login', loadChildren:'./login/login.module#LoginModule'},
+  {path:'main', loadChildren:'./main/main.module#MainModule'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],  
 })
 export class AppRoutingModule { }
