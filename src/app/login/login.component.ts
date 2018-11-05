e4bc6e3528c0565be84dbb5b5c51bfe30df4392d
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-11-05 12:15:10 +0700

    11

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenService } from '../core/service/authen.service';
-import { UtilityService } from '../core/service/utility.service';
-import { MessageConstant } from '../core/common/message.constant';
-import { NotificationService } from '../core/service/notification.service'
-import { SystemConstant } from '../core/common/system.constant'
-import { UrlConstant } from '../core/common/url.constant'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -13,9 +9,8 @@ import { UrlConstant } from '../core/common/url.constant'
 export class LoginComponent implements OnInit {
   public loading = false
   public model: any = {};
-  private returnUrl: string;
 
-  constructor(private _authentication: AuthenService, private _Utility: UtilityService, private _notification: NotificationService) { }
+  constructor(private _authentication: AuthenService) { }
 
   ngOnInit() {
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {RouterModule,Routes} from '@angular/router';
 import {AuthenService} from '../core/service/authen.service';
-import {NotificationService} from '../core/service/notification.service';
-import{UtilityService} from '../core/service/utility.service';
 import {FormsModule} from '@angular/forms'
 const loginRouter:Routes=[
 {path:'',component:LoginComponent}
@@ -16,7 +14,7 @@ const loginRouter:Routes=[
     RouterModule.forChild(loginRouter)
   ],
   declarations: [LoginComponent],
-  providers:[AuthenService,UtilityService]
+  providers:[AuthenService]
 
 })
 export class LoginModule { }
