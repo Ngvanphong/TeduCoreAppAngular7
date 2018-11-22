bf284f6be77dee0c213c97df97a16835691bd752
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2018-11-22 23:29:07 +0700

    181122v2

--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,ViewChild } from '@angular/core';
+import {ModalDirective} from 'ngx-bootstrap';
+import {SystemConstant} from '../../core/common/system.constant';
+import {DataService} from '../../core/service/data.service';
+declare var  moment:any;
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  @ViewChild('modalAddEdit') addEditModal: ModalDirective;
+  @ViewChild('avatar') avatar;
+  public pageIndex: number = 1;
+  public pageSize: number = 10;
+  public pageDisplay: number = 10;
+  public filter: string = '';
+  public totalRow: number;
+  public entity: any;
+  public users: any;
+  public allRoles:any[];
+  public myRoles:any[];
+  public roles:any[];
+  public baseFolder:string=SystemConstant.BASE_API;
 
-  constructor() { }
+  constructor(private _dataService:DataService) { }
 
   ngOnInit() {
+    this.load();
+  }
+
+  public load() {
+    this._dataService.get('/api/appUser/getlistpaging?page=' + this.pageIndex + '&pageSize=' + this.pageSize + '&filter=' + this.filter)
+      .subscribe((res: any) => {
+        this.users = res.Items;
+        this.pageIndex = res.PageIndex;
+        this.pageSize = res.PageSize;
+        this.totalRow = res.TotalRows;
+      })
   }
 
 }
--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import {Routes,RouterModule} from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap';
+import {FormsModule} from '@angular/forms';
+
 const userRouter:Routes=[
   {path:'',redirectTo:'index',pathMatch:'full'},
   {path:'index',component:UserComponent}
@@ -9,7 +12,9 @@ const userRouter:Routes=[
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild(userRouter)
+    FormsModule,
+    RouterModule.forChild(userRouter),
+    PaginationModule.forRoot(),
   ],
   declarations: [UserComponent]
 })
