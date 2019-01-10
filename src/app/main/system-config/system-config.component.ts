35775ab6badcfe1a5c270971e833e791fb48d9c8
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2019-01-10 12:17:42 +0700

    190110v3

--- src/app/main/system-config/system-config.component.ts
+++ src/app/main/system-config/system-config.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DataService } from '../../core/service/data.service';
+import { NotificationService } from '../../core/service/notification.service';
+import { MessageConstant } from '../../core/common/message.constant';
+import { ModalDirective } from 'ngx-bootstrap';
+import { NgForm } from '@angular/forms';
+declare var moment: any;
 @Component({
   selector: 'app-system-config',
   templateUrl: './system-config.component.html',
@@ -7,9 +12,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemConfigComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('addEditModal') private addEditModal: ModalDirective;
+  public entity: any;
+  public flagAdd = true;
+  public systemConfigs: any[];
+  constructor(private _dataService: DataService, private _notificationService: NotificationService) { }
 
   ngOnInit() {
+    this.search()
+  }
+  public search() {
+    this._dataService.get('/api/systemconfig/getall').subscribe((res: any) => {
+      this.systemConfigs = res
+    });
+  }
+  public showAdd() {
+    this.entity = { Status: true };
+    this.addEditModal.show();
+    this.flagAdd = true;
+  }
+
+  public editSystemModal(id: string) {
+    this._dataService.get('/api/systemconfig/detail/' + id).subscribe((res: any) => {
+      this.entity = res;
+      this.entity.Value4 = moment(this.entity.Value4).format("MM/DD/YYYY");
+      this.flagAdd = false;
+      this.addEditModal.show();
+    });
+  }
+
+  private deleteConfirm(id: string) {
+    this._dataService.delete('/api/systemconfig/delete', 'id', id).subscribe((res: any) => {
+      this.search();
+    });
+  }
+  public deleteSystem(id: string) {
+    this._notificationService.printConfirmationDialog(MessageConstant.CONFIRM_DELETE_MEG, () => this.deleteConfirm(id))
+  }
+
+  public saveChanges(form: NgForm) {
+    if (form.valid) {
+      if (this.flagAdd == true) {
+        this._dataService.post("/api/systemconfig/add", JSON.stringify(this.entity)).subscribe((res: any) => {
+          this.search();
+          this.addEditModal.hide();
+          form.resetForm();
+        })
+      }
+      else {
+        this._dataService.put("/api/systemconfig/update", JSON.stringify(this.entity)).subscribe((res: any) => {
+          this.search();
+          this.addEditModal.hide();
+          form.resetForm();
+        })
+      }
+    }
+
   }
 
 }
--- src/app/main/system-config/system-config.module.ts
+++ src/app/main/system-config/system-config.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SystemConfigComponent } from './system-config.component';
-
+import {Routes,RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap';
+import {ModalModule} from 'ngx-bootstrap'
+const systemConfigRouter:Routes=[
+  {path:'',redirectTo:'index',pathMatch:'full'},
+  {path:'index',component:SystemConfigComponent}
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(systemConfigRouter),
+    BsDatepickerModule.forRoot(), 
+    ModalModule.forRoot(),
   ],
   declarations: [SystemConfigComponent]
 })
