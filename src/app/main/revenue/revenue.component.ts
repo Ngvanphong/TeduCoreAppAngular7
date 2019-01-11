d5dfff232962edfcc7e6c97705796d89fe94b487
PHONGNGUYEN\Nguyenvan <ngvanphong2012@gamil.com>
2019-01-11 09:44:32 +0700

    191101v1

--- src/app/main/revenue/revenue.component.ts
+++ src/app/main/revenue/revenue.component.ts
@@ -14,6 +14,8 @@ export class RevenueComponent implements OnInit {
   public toDate: string = '';
   public tableData: any[];
   public chart: any
+  public totalRevenue:number;
+  public totalProfif:number;
 
   private lineChartDataRevenue: any[] = [];
 
@@ -29,6 +31,8 @@ export class RevenueComponent implements OnInit {
     this.loadRevenues();
   }
   loadRevenues() {
+    this.totalProfif=0;
+    this.totalRevenue=0;
     if (this.fromDate != '' && this.toDate != '') {
       this.fromDate = moment(this.fromDate).format('MM/DD/YYYY');
       this.toDate = moment(this.toDate).format('MM/DD/YYYY');
@@ -45,6 +49,8 @@ export class RevenueComponent implements OnInit {
           this.lineChartLabels.push(moment(item.Date).format('MM/DD/YYYY'));
           //push to table
           this.tableData = response;
+          this.totalProfif=this.totalProfif+item.Profit;
+          this.totalRevenue=this.totalRevenue+item.Revenue;
         }
         this.rawCanvat()
       });
@@ -60,12 +66,14 @@ export class RevenueComponent implements OnInit {
           {
             data: this.lineChartDataBenefif,
             borderColor: "#3cba9f",
-            fill: false
+            fill: false,
+            label:"Lợi nhuận",
           },
           {
             data: this.lineChartDataRevenue,
             borderColor: "#ffcc00",
-            fill: false
+            fill: false,
+            label:"Doanh thu",
           },
         ]
       },
@@ -85,12 +93,6 @@ export class RevenueComponent implements OnInit {
     });
 
   }
-
-
-
-
-
-
 
 
 }
